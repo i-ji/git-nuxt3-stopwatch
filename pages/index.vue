8ea910ade43ac66f6f158fc131f8491916d15f0e
i-ji <kachofugetsu1234@gmail.com>
2023-09-25 12:04:30 +0900

    ラップの順番がわかるように変更しました

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,13 @@
   <div>
     <Watch @rap="rap"></Watch>
     <!-- <Log></Log> -->
-    <ul class="bg-blue-400 text-center text-lg">
-      <li v-for="log in logs" :key="log" class="mb-2">
-        <span>{{ log.rap }}: </span>
+    <ul class="text-center text-lg w-[320px] mx-auto">
+      <li
+        v-for="(log, index) in logs"
+        :key="log"
+        class="mb-2 flex justify-between"
+      >
+        <span class="font-sans">{{ log.rap }}{{ index + 1 }}</span>
         <span>{{ log.time }}</span>
       </li>
     </ul>
